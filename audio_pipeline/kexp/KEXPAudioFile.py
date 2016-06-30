2c9462e10b913b5502f0d54cbbf56021b52504d2
Heather Huizenga <heather@kexp.org>
2016-06-30 14:33:33 -0700

    Rollback changes that broke AudioFile (whoops)

--- audio_pipeline/kexp/KEXPAudioFile.py
+++ audio_pipeline/kexp/KEXPAudioFile.py
@@ -1,7 +1,9 @@
 from audio_pipeline.util import AudioFile
 
 
-class KEXPCustomTags:
+class CustomTags:
+    obscenity = "KEXPFCCOBSCENITYRATING"
+    category = "KEXPPRIMARYGENRE"
     anchor = "KEXPAnchorStatus"
     radio_edit = "KEXPRadioEdit"
 
@@ -10,24 +12,13 @@ class KEXPAudioFile(AudioFile.BaseAudioFile):
 
     def __init__(self, file_name):
         super().__init__(file_name)
-        
-    @property
-    def anchor(self):
-        return self._get_custom_tag_(KEXPCustomTags.anchor)
 
-    @anchor.setter
-    def anchor(self, val):
-        self._set_custom_tag_(KEXPCustomTags.anchor, val)
-    
-    @property
-    def KEXPRadioEdit(self):
-        return self._get_custom_tag_(KEXPCustomTags.radio_edit)
-
-    @KEXPRadioEdit.setter
-    def KEXPRadioEdit(self, val):
-        self._set_custom_tag_(KEXPCustomTags.radio_edit, val)
+        self.obscenity = self.format.custom_tag(CustomTags.obscenity, self.audio)
+        self.category = self.format.custom_tag(CustomTags.category, self.audio)
+        self.anchor = self.format.custom_tag(CustomTags.anchor, self.audio)
+        self.radio_edit = self.format.custom_tag(CustomTags.radio_edit, self.audio)
 
     def track(self):
         track = super().track()
-        track += [self.anchor, self.KEXPRadioEdit]
+        track += [self.obscenity, self.category, self.anchor, self.radio_edit]
         return track
--- audio_pipeline/util/AudioFile.py
+++ audio_pipeline/util/AudioFile.py
@@ -1,7 +1,5 @@
 import mutagen
-import copy
 
-from audio_pipeline import Constants
 from .format import Vorbis
 from .format import AAC
 from .format import ID3
@@ -9,16 +7,20 @@ from . import Exceptions
 from . import Tag
 
 
+class CustomTags:
+    item_code = "ITEMCODE"
+
+
 class BaseAudioFile:
 
     vorbis = Vorbis.Format
     id3 = ID3.Format
     aac = AAC.Format
-
+    
     def __init__(self, file_name):
         self._format = None
         self.file_name = file_name
-
+        
         try:
             self.audio = mutagen.File(file_name)
             if not self.audio:
@@ -27,185 +29,41 @@ class BaseAudioFile:
             # if there's an error opening the file (probably not an audio file)
             # propagate the resulting exception on up
             raise e
-
+            
         for type in self.audio.mime:
             # get the appropriate tag Format for this file type
             if type in Tag.Formats.mime_map:
                 t = Tag.Formats.mime_map[type]
                 self.format = t
                 break
-
+                
         if not self.format:
             # Can't process this type of audio file; raise UnsupportedFileType error
             raise Exceptions.UnsupportedFiletypeError(file_name)
-
+            
         # get tags
-
+        
         #######################
         #   release-level tags
         #######################
-
-        self._mbid = None        
-        self._album = None
-        self._album_artist = None
-        self._release_date = None
-        self._label = None
-
+        
+        self.mbid = self.format.mbid(self.audio)
+        self.album = self.format.album(self.audio)
+        self.album_artist = self.format.album_artist(self.audio)
+        self.release_date = self.format.release_date(self.audio)
+        self.label = self.format.label(self.audio)
+                
         #######################
         #   track-level tags
         #######################
 
-        self._title = None
-        self._artist = None
-        self._disc_num = None
-        self._track_num = None
-        self._length = None
-        self.custom_tags = {}
+        self.title = self.format.title(self.audio)
+        self.artist = self.format.artist(self.audio)
+        self.disc_num = self.format.disc_num(self.audio)
+        self.track_num = self.format.track_num(self.audio)
+        self.length = self.format.length(self.audio)
         
-    #######################
-    #   release-level tags
-    #######################
-
-    @property
-    def mbid(self):
-        return self._get_tag_(self._mbid, self.format.mbid)
-
-    @mbid.setter
-    def mbid(self, val):
-        self._set_tag_(self._mbid, self.format.mbid, val)
-
-    @property
-    def album(self):
-        return self._get_tag_(self._album, self.format.album)
-
-    @album.setter
-    def album(self, val):
-        self._set_tag_(self._album, self.format.album, val)
-
-    @property
-    def album_artist(self):
-        return self._get_tag_(self._album_artist, self.format.album_artist)
-
-    @album_artist.setter
-    def album_artist(self, val):
-        self._set_tag_(self._album_artist, self.format.album_artist, val)
-
-    @property
-    def release_date(self):
-        return self._get_tag_(self._release_date, self.format.release_date)
-
-    @release_date.setter
-    def release_date(self, val):
-        self._set_tag_(self._release_date, self.format.release_date, val)
-
-    @property
-    def label(self):
-        return self._get_tag_(self._label, self.format.label)
-
-    @label.setter
-    def label(self, val):
-        self._set_tag_(self._label, self.format.label, val)
-
-    #######################
-    #   track-level tags
-    #######################
-    
-    @property
-    def title(self):
-        return self._get_tag_(self._title, self.format.title)
-
-    @title.setter
-    def title(self, val):
-        self._set_tag_(self._title, self.format.title, val)
-
-    @property
-    def artist(self):
-        return self._get_tag_(self._artist, self.format.artist)
-
-    @artist.setter
-    def artist(self, val):
-        self._set_tag_(self._artist, self.format.artist, val)
-
-    @property
-    def disc_num(self):
-        return self._get_tag_(self._disc_num, self.format.disc_num)
-
-    @disc_num.setter
-    def disc_num(self, val):
-        self._set_tag_(self._disc_num, self.format.disc_num, val)
-
-    @property
-    def track_num(self):
-        return self._get_tag_(self._track_num, self.format.track_num)
-
-    @track_num.setter
-    def track_num(self, val):
-        self._set_tag_(self._track_num, self.format.track_num, val)
-
-    @property
-    def length(self):
-        return self._get_tag_(self._length, self.format.length)
-
-    @length.setter
-    def length(self, val):
-        self._set_tag_(self._length, self.format.length, val)
-
-
-    ##################
-    # Custom Tags
-    ##################
-    
-    @property
-    def item_code(self):
-        return self._get_custom_tag_(Constants.custom_tags["item_code"])
-
-    @item_code.setter
-    def item_code(self, val):
-        self._set_custom_tag_(Constants.custom_tags["item_code"], val)
-    
-    @property
-    def obscenity(self):
-        return self._get_custom_tag_(Constants.custom_tags["obscenity"])
-
-    @obscenity.setter
-    def obscenity(self, val):
-        self._set_custom_tag_(Constants.custom_tags["obscenity"], val)
-
-    @property
-    def category(self):
-        return self._get_custom_tag_(Constants.custom_tags["category"])
-
-    @category.setter
-    def category(self, val):
-        self._set_custom_tag_(Constants.custom_tags["category"], val)
-
-    def _get_tag_(self, local_tag, format_tag):
-        if not local_tag:
-            local_tag = format_tag(self.audio)
-        return local_tag
-
-    def _set_tag_(self, local_tag, format_tag, val):
-        if isinstance(val, type(format_tag)):
-            local_tag = copy.deepcopy(val)
-        elif local_tag:
-            local_tag.value = val
-        else:
-            local_tag = format_tag({})
-            local_tag.value = val
-
-    def _get_custom_tag_(self, local_tag_name):
-        if not self.custom_tags.get(local_tag_name):
-            self.custom_tags[local_tag_name] = self.format.custom_tag(local_tag_name, self.audio)
-        return self.custom_tags[local_tag_name]
-
-    def _set_custom_tag_(self, local_tag_name, val):
-        if isinstance(val, type(self.custom_tags[local_tag_name])):
-            self.custom_tags[local_tag_name] = copy.deepcopy(val)
-        elif self.custom_tags.get(local_tag_name):
-            self.custom_tags[local_tag_name].value = val
-        else:
-            self.custom_tags[local_tag_name] = self.format.custom_tag(local_tag_name, {})
-            self.custom_tags[local_tag_name].value = val
+        self.item_code = self.format.custom_tag(CustomTags.item_code, self.audio)
 
     @property
     def format(self):
@@ -235,7 +93,7 @@ class BaseAudioFile:
             yield item
             
     def track(self):
-        return [self.track_num, self.title, self.artist, self.length, self.obscenity, self.category, self.item_code]
+        return [self.track_num, self.title, self.artist, self.length]
     
     def release(self):
-        return [self.album_artist, self.album, self.label, self.disc_num, self.release_date, self.mbid]
+        return [self.album_artist, self.album, self.label, self.disc_num, self.release_date, self.mbid, self.item_code]
